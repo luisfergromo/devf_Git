886efb4753f2739bcc18b727f3bb648af67687bf
Your Name <you@example.com>
2017-09-22 21:28:15 -0500

    .

---
+++ src/Alfabetico.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+
+/**
+ * @author egual
+ *
+ */
+public class Alfabetico {
+	
+	import java.util.ArrayList;
+	import java.util.HashSet;
+
+	public class Logic {
+
+	    public static void studList() {
+	        ArrayList<String> stud = new ArrayList<>();
+	        ArrayList<Double> uspe = new ArrayList<>();
+	        
+
+	        ArrayList<String> list = sortA(stud,uspe);
+	        for (String s:list) {
+	            View.printer(s);
+	        }
+	        View.printer("-");
+	        for (int i = list.size()-1; i >= 0; i--) {
+	            String s = list.get(i);
+	            View.printer(s);
+	        }
+
+	    }
+
+	    private static ArrayList<String> sortA(ArrayList<String> st, ArrayList<Double> us) {
+	        ArrayList<String> list = new ArrayList<>();
+	        String s;
+	        for (int i = 0; i < st.size(); i++) {
+	            s = st.get(i) + ": " + us.get(i);
+	            list.add(s);
+	        }
+	        HashSet<String> set = new HashSet<>(list);
+	        list.clear();
+	        list.addAll(set);
+	        return list;
+	    }
+
+	}
+}
