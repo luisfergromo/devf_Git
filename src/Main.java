8583e5c3890e99f6e4ce1e17169db4823a2f429a
luisromo <changoluis@hotmail.com>
2017-09-22 20:41:26 -0500

    last

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
 
+class Product {
+    private int productID;
+    private String productName;
+    private int productPrice;
+    private String productType;
+}
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        ArrayList<Product> productList = new ArrayList<Product>();
+        productList.add(new Product(1,"Libreta",129,"escribir"));
+        productList.add(new Product(2,"Lápiz",5,"escribir"));
+        productList.add(new Product(3,"Pluma",7,"escribir"));
     }
 }
